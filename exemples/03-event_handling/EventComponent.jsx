1721d652723bd3ad7b60f6fa8b99fc13cb5a3dd7
Julien M'Poy <julien.mpoy@gmail.com>
2016-12-29 00:18:02 +0100

    Message d'évènements en console pour l'exemple

--- exemples/03-event_handling/EventComponent.jsx
+++ exemples/03-event_handling/EventComponent.jsx
@@ -4,19 +4,17 @@ class MyComponent extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick(e) {
-        var out = document.getElementById('event-output');
-        out.innerHTML = out.innerHTML + 'You toggled me! How kind you are!<br />';
+        console.log('You toggled me! How kind you are!<br />');
     }
     render() {
         return (
             <div>
                 <h1>Event Handling</h1>
                 <p>Enjoy that toggle button!</p>
-                    <label className="switch">
-                        <input type="checkbox"></input>
-                        <div className="slider round" onClick={this.handleClick}></div>
-                    </label>
-                    <p id="event-output"></p>
+                <label className="switch">
+                    <input type="checkbox"></input>
+                    <div className="slider round" onClick={this.handleClick}></div>
+                </label>
             </div>
         );
     }
