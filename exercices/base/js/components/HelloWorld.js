9ffe404b0e63eb7b9702624d45467ba04001b3ff
Julien M'Poy <julien.mpoy@gmail.com>
2018-02-16 14:28:45 +0100

    Added sass to the exercises' minimalistic boilerplate

--- exercices/base/js/components/HelloWorld.js
+++ exercices/base/js/components/HelloWorld.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HelloWorld = props => {
   return (
-    <div>
+    <div className="container">
       <h1>You started the boilerplate!</h1>
       <p>This is the base project skeleton.</p>
       <p>Use this minimaltistic boilerplate to do the given exercices.</p>
--- exercices/base/webpack.config.js
+++ exercices/base/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   // Where is the entry script
-  entry: ['./js/index.js'],
+  entry: ['./js/index.js', './sass/app.scss'],
   // Where the resulting compiled JavaScript has to go
   output: {
     path: path.resolve(__dirname, 'dist'), // Create a 'dist' folder that contains resulting js and HTML
@@ -21,9 +22,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         // Processors/Plugins to for JavaScript (babel in our case)
-        use: {
-          loader: 'babel-loader',
-        },
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
       },
       // We use an HTML template for oure single page app
       {
@@ -35,13 +38,25 @@ module.exports = {
           },
         ],
       },
+      // We compile SCSS into CSS
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
     ],
   },
   plugins: [
     // Where is our template and where (in 'dist') do we place it.
     new HtmlWebPackPlugin({
-      template: './index.html',
+      template: './templates/index.html',
       filename: './index.html',
+    }),
+    // Where our resulting CSS has to be placed
+    new ExtractTextPlugin({
+      filename: 'css/app.css',
     }),
   ],
 };
