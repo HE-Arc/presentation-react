e346ae3cf4ed2843dea44243f10c4580190dabd5
Julien M'Poy <julien.mpoy@gmail.com>
2018-02-16 13:15:03 +0100

    Added minimalistic boilerplate

---
+++ exercices/base/js/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HelloWorld from './components/HelloWorld.js';
+
+const App = props => {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
---
+++ exercices/base/js/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HelloWorld = props => {
+  return (
+    <div>
+      <h1>You started the boilerplate!</h1>
+      <p>This is the base project skeleton.</p>
+      <p>Use this minimaltistic boilerplate to do the given exercices.</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
